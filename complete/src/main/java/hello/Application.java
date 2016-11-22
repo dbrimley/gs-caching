725aaf2c0781ae9a983198f92c3f9ce188a844af
David Brimley <dave.brimley@gmail.com>
2016-11-22 21:07:24 +0000

    Include a HazelcastClient bean

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,8 +1,11 @@
 package hello;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,6 +13,11 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public HazelcastInstance hazelcastInstance(){
+        return HazelcastClient.newHazelcastClient();
     }
 
 }
